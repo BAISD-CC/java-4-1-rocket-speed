35a75f3cb848aa9a8b466d9842369b4ab2eedf00
Justin Kruskie <kruskiej@baisd.net>
2024-02-26 15:50:24 +0000

    Create assignment

---
+++ RocketSpeed.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class RocketSpeed {
+
+    public static void main(String[] args) {
+
+        // Variables for current speed, total speed, max seconds, and current seconds.
+        // Create a Scanner object for keyboard input.
+        // Get the maximum number of seconds.
+
+        // Validate the input.
+            // Check to see if the input is larger than 0.
+              // If it is not, tell the user to enter another number.
+              // Otherwise, accept the input.
+
+        // Close the keyboard
+        // Initialize the seconds counter to 1.
+        // Initialize the speed accumulator for the first second of the burn (100m/s).
+        // Initialize the total speed accumulator.
+        // Display the report header.
+
+        // Display the speed report using a loop.
+            // Display the seconds number and speed of the rocket.
+            // Increment for the next second.
+            // Accumulate the total speed.
+            // Double the speed.
+
+        // Display the final speed.
+        // Close the program
+
+    }
+
+}
